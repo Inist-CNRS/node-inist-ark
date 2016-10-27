be5ca16e1d78ff4e83ccbdd06d0d5daa58ac54c7
Stéphane Gully <stephane.gully@gmail.com>
2016-10-28 00:01:16 +0200

    more examples

---
+++ examples/parse.js
@@ -0,0 +1,4 @@
+var InistArk = require('..');
+
+var ark = new InistArk();
+console.log(ark.parse('ark:/67375/39D-PJP969M7-L'));
---
+++ examples/validate.js
@@ -0,0 +1,5 @@
+var InistArk = require('..');
+
+var ark = new InistArk();
+console.log(ark.validate('ark:/67375/39D-PJP969M7-L'));
+console.log(ark.validate('ark:/67375/39D-PJP969M8-L'));
